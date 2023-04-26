8393132394a24d696052a8f240d5fb48f866d7d5
Pranami Rutvik <123811033+RutvikPranami@users.noreply.github.com>
2023-04-26 23:16:45 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,17 +2,36 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-// Importing products from products.json file
 const userDetails = JSON.parse(
   fs.readFileSync(`${__dirname}/data/userDetails.json`)
 );
 
-//Middlewares
+
 app.use(express.json());
 
-// Write POST endpoint for registering new user
 
-// GET endpoint for sending the details of users
+
+app.post("/api/v1/details", (req, res) => {
+  const newId = userDetails[userDetails.length - 1].id + 1;
+  const { name, mail, number } = req.body;
+  const newUser = { id: newId, name, mail, number };
+  userDetails.push(newUser);
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        message: "User registered successfully",
+        data: {
+          userDetails: newUser,
+        },
+      });
+    }
+  );
+});
+
+
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
     status: "Success",
@@ -23,7 +42,6 @@ app.get("/api/v1/details", (req, res) => {
   });
 });
 
-// GET endpoint for sending the products to client by id
 app.get("/api/v1/userdetails/:id", (req, res) => {
   let { id } = req.params;
   id *= 1;
